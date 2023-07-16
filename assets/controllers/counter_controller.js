c3ff0fa8b90ae4988c19dba909ab72805945fd1a
argentum88 <argentimgipan@gmail.com>
2023-07-16 16:09:39 +0500

    Try targets feature

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static targets = ['count'];
+
     connect() {
-        this.element.textContent = 'You have clicked me 0 times';
         this.count = 0;
 
         this.element.addEventListener('click', () => {
             this.count++;
-            this.element.textContent = `You have clicked me ${this.count} times`;
+            this.countTarget.innerHTML = this.count;
         });
     }
 }
