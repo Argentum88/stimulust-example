bbc7bf3eb4f3e63ff0a888b2cec595e704df4eb0
argentum88 <argentimgipan@gmail.com>
2023-07-16 13:53:40 +0500

    Add click listener

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -3,5 +3,11 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     connect() {
         this.element.textContent = 'You have clicked me 0 times';
+        this.count = 0;
+
+        this.element.addEventListener('click', () => {
+            this.count++;
+            this.element.textContent = `You have clicked me ${this.count} times`;
+        });
     }
 }
