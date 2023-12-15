3ce80501b4917c5201146545de2b0a4e9d69c228
argentum88 <argentimgipan@gmail.com>
2023-12-15 22:55:05 +0500

    Try value api callback

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -7,6 +7,9 @@ export default class extends Controller {
 
     connect() {
         this.selectTarget.classList.add('d-none')
+    }
+
+    colorIdValueChanged() {
         this.setSelectColor(this.colorIdValue)
     }
 
